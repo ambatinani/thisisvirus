f386a437150a7cc20bd04b06ff059e53a0904c61
Navaneeth <100183258+ambatinani@users.noreply.github.com>
2023-02-26 14:47:48 +0530

    Update account.php

--- account.php
+++ account.php
@@ -38,11 +38,9 @@ if (@$_GET['w']) {
 include_once 'dbConnection.php';
 ?>
 <body>
-<div class="header">
-<div class="row">
-<div class="col-lg-6">
-<span class="logo">Quiz App</span></div>
-<div class="col-md-4 col-md-offset-2">
+
+
+
  <?php
 include_once 'dbConnection.php';
 
@@ -53,41 +51,13 @@ if (!(isset($_SESSION['username']))) {
     $username = $_SESSION['username'];
     
     include_once 'dbConnection.php';
-    echo '<span class="pull-right top title1" ><span style="color:white"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <span class="log log1" style="color:lightyellow">' . $username . '&nbsp;&nbsp;|&nbsp;&nbsp;<a href="logout.php?q=account.php" style="color:lightyellow"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Logout</button></a></span>';
+   
 }
 ?>
-</div>
-</div></div>
+
+
 <div class="bg">
-<nav class="navbar navbar-default title1">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#"><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></a>
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li <?php
-if (@$_GET['q'] == 1)
-    echo 'class="active"';
-?> ><a href="account.php?q=1"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home<span class="sr-only">(current)</span></a></li>
-        <li <?php
-if (@$_GET['q'] == 2)
-    echo 'class="active"';
-?>><a href="account.php?q=2"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;My History</a></li>
-    <li <?php
-if (@$_GET['q'] == 3)
-    echo 'class="active"';
-?>><a href="account.php?q=3"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Leaderboard</a></li></ul>
-            
-      </div>
-  </div>
-</nav>
+
 <div class="container">
 <div class="row">
 <div class="col-md-12">
