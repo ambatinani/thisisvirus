bc99e96390eb7af8edc85d01a6aa68f37ef94a2f
Navaneeth <100183258+ambatinani@users.noreply.github.com>
2023-04-11 15:43:40 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,3 @@
 <?php
-	$con= new mysqli('localhost','root','UecS3dwH5dA8Ey','quiz')or die("Could not connect to mysql".mysqli_error($con));
+	$con= new mysqli('localhost','root','k53RMp599Gct5R','quiz')or die("Could not connect to mysql".mysqli_error($con));
 ?>
